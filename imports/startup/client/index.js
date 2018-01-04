37b0aaaf2620af60a25a34f42b9677fb336c7420
mattperkins <github@mattperkins.com>
2018-01-04 01:18:00 +0000

    add test graphql schema

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Meteor } from "meteor/meteor";
 import { render } from "react-dom";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
 import App from "../../ui/App";
 
 Meteor.startup(() => {
---
+++ imports/startup/server/index.js
@@ -0,0 +1,29 @@
+// console.log("Hello, World!");
+
+import { createApolloServer } from "meteor/apollo";
+import { makeExecutableSchema } from "graphql-tools";
+
+//  Graphql Schema data structure of the query
+const typeDefs = `
+    type Query {
+        yo: String
+    }
+`;
+
+// Server Side Method - the query data
+const resolvers = {
+  Query: {
+    yo() {
+      return "yo yo yo!";
+    }
+  }
+};
+
+// create schema - connection
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+//  pass into apollo server
+createApolloServer({ schema });
