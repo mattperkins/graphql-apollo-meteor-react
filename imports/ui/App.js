a40c3b9bbd043f92930724b0756385ddd414e459
mattperkins <github@mattperkins.com>
2018-01-04 02:39:04 +0000

    add apolloprovider

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,5 +1,12 @@
-import React from "react";
+// import gql from "graphql-tag";
+// import { graphql } from "react-apollo";
 
 const App = () => <h1>Hello UI</h1>;
+
+// const yoQuery = gql`
+//   {
+//     yo
+//   }
+// `;
 
 export default App;
